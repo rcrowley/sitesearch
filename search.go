d207a75e6aeffe16b6fc5b02206fd6a76ac3971c
Richard Crowley <r@rcrowley.org>
2024-02-05 14:30:17 -0800

    A start on the search result page

---
+++ search.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/rcrowley/sitesearch/html"
+	"github.com/rcrowley/sitesearch/index"
+	"golang.org/x/net/html/atom"
+)
+
+const IdxFilename = "sitesearch.idx"
+
+func Search(q string) (*html.Node, error) {
+
+	idx, err := index.Open(IdxFilename)
+	if err != nil {
+		return nil, err
+	}
+	defer idx.Close()
+
+	result, err := idx.Search(q)
+
+	n := &html.Node{
+		DataAtom: atom.Pre,
+		Data:     "pre",
+		Type:     html.ElementNode,
+	}
+	n.AppendChild(&html.Node{
+		Data: fmt.Sprint(result),
+		Type: html.TextNode,
+	})
+	return n, nil
+}
+
+func SearchHandler(ctx context.Context, req events.LambdaFunctionURLRequest) (resp events.LambdaFunctionURLResponse, err error) {
+	panic("not implemented")
+}
---
+++ search_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/rcrowley/sitesearch/html"
+	"github.com/rcrowley/sitesearch/index"
+)
+
+func TestSearch(t *testing.T) {
+	must(os.RemoveAll(IdxFilename))
+	idx := must2(index.Open(IdxFilename))
+	must(idx.IndexHTMLFile("index/test.html"))
+	must(idx.Close())
+	defer os.RemoveAll(IdxFilename)
+
+	n, err := Search("cool")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s := html.String(n)
+	if !strings.Contains(s, "1 matches") {
+		t.Fatal(s)
+	}
+	if !strings.Contains(s, "1. index/test.html") {
+		t.Fatal(s)
+	}
+
+}
