adbea8f366a585b0a970f02b259e4658b826e204
Richard Crowley <r@rcrowley.org>
2024-02-05 10:49:08 -0800

    Shell out to zip(1) to add the index to the zip file

--- zip.go
+++ zip.go
@@ -3,80 +3,37 @@
 package main
 
 import (
-	"archive/zip"
-	"bytes"
 	_ "embed"
 	"fmt"
-	"io"
-	"log"
 	"os"
-	"path/filepath"
+	"os/exec"
 )
 
 // Zip writes the starting-point zip file that contains the compiled Lambda
 // function, named bootstrap per the Lambda provided.al2 runtime requirement,
 // to disk, and adds the file with the given relative pathname to it.
-func Zip(pathname string) (err error) {
-	var (
-		fIdx, fZip *os.File
-		r          *zip.Reader
-		rBin       io.ReadCloser
-		w          *zip.Writer
-		wBin, wIdx io.Writer
-	)
+func Zip(pathname string) error {
 
-	if r, err = zip.NewReader(bytes.NewReader(bootstrapZip), int64(len(bootstrapZip))); err != nil {
-		log.Print(err)
-		return
+	f, err := os.Create("sitesearch.zip")
+	if err != nil {
+		return err
 	}
-
-	if fZip, err = os.Create("sitesearch.zip"); err != nil {
-		return
-	}
-	w = zip.NewWriter(fZip)
-
-	if wBin, err = w.Create("bootstrap"); err != nil {
-		goto Error
-	}
-	for _, f := range r.File {
-		if f.Name != "bootstrap" {
-			panic(fmt.Sprintf(
-				"there should not be any files except bootstrap in this zip file; found %s",
-				f.Name,
-			))
+	if _, err := f.Write(bootstrapZip); err != nil {
+		if err2 := f.Close(); err2 != nil {
+			return fmt.Errorf("%w %w", err, err2)
 		}
-		if rBin, err = f.Open(); err != nil {
-			goto Error
-		}
-		if _, err = io.Copy(wBin, rBin); err != nil {
-			goto Error
-		}
-		if err = rBin.Close(); err != nil {
-			goto Error
-		}
+		return err
+	}
+	if err := f.Close(); err != nil {
+		return err
 	}
 
-	if fIdx, err = os.Open(pathname); err != nil {
-		return
-	}
-	if wIdx, err = w.Create(filepath.Base(pathname)); err != nil {
-		goto Error
-	}
-	if _, err = io.Copy(wIdx, fIdx); err != nil {
-		goto Error
-	}
-	if err = fIdx.Close(); err != nil {
-		goto Error
+	cmd := exec.Command("zip", "-X", "-r", "sitesearch.zip", pathname)
+	if err := cmd.Run(); err != nil {
+		return err
 	}
 
-Error:
-	if err = w.Close(); err != nil {
-		return
-	}
-	if err = fZip.Close(); err != nil {
-		return
-	}
-	return
+	return nil
 }
 
 //go:generate env GOARCH=arm64 GOOS=linux go build -o bootstrap -tags lambda
--- zip_test.go
+++ zip_test.go
@@ -1,32 +1,69 @@
 package main
 
 import (
+	"archive/zip"
+	"log"
 	"os"
-	"os/exec"
 	"testing"
+
+	"github.com/rcrowley/sitesearch/index"
 )
 
 func TestZip(t *testing.T) {
-	if err := os.WriteFile("sitesearch.idx", []byte("not a real index, just testing zip file I/O"), 0666); err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove("sitesearch.idx")
+
+	// Known-good sitesearch.idx to add to the zip file.
+	must(os.RemoveAll("sitesearch.idx"))
+	idx := must2(index.Open("sitesearch.idx"))
+	must(idx.Close())
+	defer os.RemoveAll("sitesearch.idx")
+
 	if err := Zip("sitesearch.idx"); err != nil {
 		t.Fatal(err)
 	}
 	defer os.Remove("sitesearch.zip")
-	stdout, err := exec.Command("unzip", "-l", "sitesearch.zip").Output()
+
+	r, err := zip.OpenReader("sitesearch.zip")
 	if err != nil {
-		t.Fatal(err)
+		log.Fatal(err)
 	}
-	if string(stdout) != `Archive:  sitesearch.zip
-  Length      Date    Time    Name
----------  ---------- -----   ----
-  1951131  00-00-1980 00:00   bootstrap
-       43  00-00-1980 00:00   sitesearch.idx
----------                     -------
-  1951174                     2 files
-` {
-		t.Fatal(string(stdout))
+	defer r.Close()
+	var count int
+	for _, f := range r.File {
+		fi := f.FileInfo()
+		switch f.Name { // f.Name is the relative pathname; fi.Name() is the basename
+		case "bootstrap":
+			count++
+			if fi.Size() < 1000000 {
+				t.Fatalf("%s is suspiciously small (%d bytes)", fi.Name(), fi.Size())
+			}
+		case "sitesearch.idx/":
+			count++
+		case "sitesearch.idx/index_meta.json":
+			count++
+			if fi.Size() != 42 {
+				t.Fatalf("%s should contain 42 bytes but contains %d bytes", fi.Name(), fi.Size())
+			}
+		case "sitesearch.idx/store/":
+			count++
+		case "sitesearch.idx/store/root.bolt":
+			count++
+			if fi.Size() != 65536 {
+				t.Fatalf("%s should contain 65536 bytes but contains %d bytes", fi.Name(), fi.Size())
+			}
+		default:
+			t.Fatal(fi.Name())
+		}
 	}
+	if count != 5 {
+		t.Fatalf("only found %d out of the expected 5 files in sitesearch.zip", count)
+	}
+
+	/*
+		stdout, err := exec.Command("unzip", "-l", "sitesearch.zip").Output()
+		if err != nil {
+			t.Fatal(err)
+		}
+		os.Stdout.Write(stdout)
+	*/
+
 }
