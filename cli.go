71349b16f6ac6808cd3a884a1bb8245be04edccf
Richard Crowley <r@rcrowley.org>
2024-02-05 10:59:38 -0800

    main getting as far as creating the custom zip file

--- cli.go
+++ cli.go
@@ -3,16 +3,49 @@
 package main
 
 import (
-	_ "embed"
+	"flag"
+	"fmt"
 	"log"
+	"os"
+
+	"github.com/rcrowley/sitesearch/index"
 )
 
-//go:generate env GOARCH=arm64 GOOS=linux go build -o bootstrap -tags lambda
-//go:generate touch -t 202402040000.00 bootstrap
-//go:generate zip -X lambda.zip bootstrap
-//go:embed lambda.zip
-var bin []byte
+func init() {
+	log.SetFlags(0)
+}
 
 func main() {
-	log.Print("main")
+	//output := flag.String("o", "-", "write to this file instead of standard output")
+	// TODO -n Lambda function name, no -o (?), -r AWS region
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, `Usage: sitesearch [-o <output>] <input>[...]
+  -o <output>   write to this file instead of standard output
+  <input>[...]  pathname to one or more input HTML or Markdown files
+`)
+	}
+	flag.Parse()
+
+	const idxFilename = "sitesearch.idx"
+
+	idx := must2(index.Open(idxFilename))
+	defer idx.Close()
+
+	must(idx.IndexHTMLFiles(flag.Args()))
+
+	zipFilename := must2(Zip(idxFilename))
+
+	log.Print(zipFilename) // TODO upload zipFilename to Lambda and set a function URL
+
+}
+
+func must(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func must2[T any](v T, err error) T {
+	must(err)
+	return v
 }
